94aebedc1f371705a84742ab4d44a85fbf50b058
kate-popovich <verokatya2002@gmail.com>
2024-11-29 01:10:13 +0300

    add doctor model, create superuser

--- dentistry/models.py
+++ dentistry/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor (models.Model):
+  surname = models.CharField(max_length=20, unique=False, help_text="Enter the surname")
+  name = models.CharField(max_length=20, unique=False, help_text="Enter the name")
+  middle_name = models.CharField(max_length=20, unique=False, help_text="Enter the middle name")
+  birthday = models.DateField(unique=False, help_text="Enter your birthday")
+  snils = models.IntegerField(unique=True, max_length=11, help_text="Enter your SNILS")
+  inn = models.IntegerField(unique=True, max_length=12, help_text="Enter your INN")
